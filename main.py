bc4a84e55ce1f0a468c6ea5e460af5c8b50558a5
Daniel Wieczorek <daniel.pazur@gmail.com>
2021-09-27 20:45:54 +0200

    test

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ class NeuralNetwork:
             self.weights[i] = self.weights[i] - 0.2 * weighted_delta        # Adjusting the weights
 
 
-# todo: File handling
+# todo: File handling AAAA
 # todo: More advanced user features
 #   Replace np.ones data with custom values
 # todo: Fix fit() for multi-layer nets
