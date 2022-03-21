555b052bd4e80eb390cec8dbfd76e8d4557ed161
student <student@L405-2.carbon.cti.p.lodz.pl>
2022-03-21 12:15:14 +0100

    Implemented colour validation

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ class NeuralNetwork:
 
     def blankData(self):
         self.dataset.clear()
-        self.dataset.append(
-            Data(np.ones((1, self.inputSize)), np.ones((1, self.outputSize))))
+        # self.dataset.append(
+        #     Data(np.ones((1, self.inputSize)), np.ones((1, self.outputSize))))
 
     def getOutputLayer(self):
         return self.values[-1]
@@ -117,6 +117,59 @@ class NeuralNetwork:
             print(f"Input:{data.input[0]}")
             print(f"Output:{data.output[0]}\n")
 
+    def addSampleColour(self, r: float, g: float, b: float, colour: int):
+        network.dataset.append(
+            Data(np.zeros((1, network.inputSize)), np.zeros((1, network.outputSize))))
+
+        self.dataset[-1].input[0][0] = r
+        self.dataset[-1].input[0][1] = g
+        self.dataset[-1].input[0][2] = b
+
+        print(f"colour = {colour}")
+        self.dataset[-1].output[0][colour-1] = 1
+
+        print(f"Appending input{self.dataset[-1].input[0]}, output = {self.dataset[-1].output[0]}")
+
+    def loadColourFile(self, filename):
+        with open(filename, 'r') as handle:
+            data = [*map(float, handle.read().split())]
+
+        flag = 0
+        for i in range(len(data)):
+            if flag == 0:
+                print(f"0, {data[i]}")
+                r = data[i]
+                flag += 1
+            elif flag == 1:
+                print(f"1, {data[i]}")
+                g = data[i]
+                flag += 1
+            elif flag == 2:
+                print(f"2, {data[i]}")
+                b = data[i]
+                flag += 1
+            elif flag == 3:
+                print(f"3, {data[i]}")
+                out = data[i]
+                self.addSampleColour(r, g, b, int(out))
+                flag = 0
+
+    def validateColours(self):
+        total = 0
+        correct = 0
+        for sample in self.dataset:
+            total += 1
+            result = self.predict(sample.input)
+            print(result)
+            print(sample.output[0])
+            print(f"Argmax = {np.argmax(result[0])}")
+
+            if np.argmax(result[0]) == np.argmax(sample.output[0]):
+                print("Correct!")
+                correct += 1
+            else:
+                print(f"WRONG!, {np.argmax(result[0])} != {np.argmax(sample.output[0])}")
+        return float(correct/total * 100)
 
 
 
@@ -178,8 +231,9 @@ while True:
             int(input("Enter min weight value")),
             int(input("Enter max weight value")))
     if operation == 2:
-        for i in range(100):
+        for i in range(50):
             network.fit()
+            print("\n")
     if operation == 3:
         network.display()
     if operation == 4:
@@ -195,11 +249,10 @@ while True:
     if operation == 8:
         network.addSampleManual()
         network.displayDataset()
-
-
-
-
-
+    if operation == 9:
+        network.loadColourFile("colours.txt")
+    if operation == 10:
+        print(f"{network.validateColours()}%")
 
 
 # inputData = np.ones(3)
