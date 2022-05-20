65ae61bd13c84cbc1218b043aff66841433977d6
Daniel Wieczorek <daniel.pazur@gmail.com>
2022-05-20 15:05:44 +0200

    Implemented mask generation and adjustments to ValueLayer class

--- NetworkStructure/ValueLayer.py
+++ NetworkStructure/ValueLayer.py
@@ -1,19 +1,19 @@
 import numpy as np
 
-import ActivationFunctions.Sigmoid
 from ActivationFunctions.NoFunction import NoFunction
 from ActivationFunctions.ReLU import ReLU
 from ActivationFunctions.Sigmoid import Sigmoid
 
 
 class ValueLayer:
-    def __init__(self, size, activationFunction=NoFunction):
-        print(f"Size = {size}")
+    def __init__(self, size, activationFunction=NoFunction, dropoutOdds=1.0):
         self.values = np.zeros((1, size))
         self.mask = np.ones((1, size))
         self.delta = np.zeros((1, size))
-
         self.activationFunction = activationFunction
+
+        self.dropoutOdds = dropoutOdds
+        self.generateMask(self.dropoutOdds)
 
     def generateMask(self, probability: float) -> None:
         self.mask = np.random.rand(1, self.mask.size) < probability
@@ -21,8 +21,15 @@ class ValueLayer:
         # res /= probability
         # print(res)
 
-    def getMasked(self):
-        return np.multiply(self.values, self.mask)
+    def applyDropout(self):
+        self.applyMask()
+        self.adjustValues()
+
+    def applyMask(self):
+        self.values = np.multiply(self.values, self.mask)
+
+    def adjustValues(self):
+        self.values = np.multiply(self.values, 1 / self.dropoutOdds)
 
     def getSize(self):
         return self.values.size
@@ -40,13 +47,14 @@ class ValueLayer:
         return self.activationFunction.derivative(self.values)
 
 
-#
-# layer = ValueLayer(10, NoFunction)
+# layer = ValueLayer(10, NoFunction, 0.5)
 # layer.values[0][4] = 3
 # layer.values[0][3] = -3
 # layer.values[0][2] = 8
 # layer.values[0][1] = 5
-# print(layer)
+# print("Layer: " + str(layer))
 # layer.applyMethod()
-# print(layer)
-# print(layer.getAfterDeriv())
+# print("Layer after activation method: " + str(layer))
+# layer.applyDropout()
+# print("Mask: " + str(layer.mask))
+# print("Layer after dropout: " + str(layer))
