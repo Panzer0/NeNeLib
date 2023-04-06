9fef063cf89c5eddbdbc3c127d7659f1534ce6a8
Daniel Wieczorek <daniel.pazur@gmail.com>
2023-04-07 00:43:21 +0200

    Made unnecessary methods into functions

--- MNISTHandler.py
+++ MNISTHandler.py
@@ -5,6 +5,19 @@ MAX_TRAIN_SIZE = 60000
 MAX_TEST_SIZE = 10000
 
 
+def get_adjusted_input(array):
+    return array.reshape(array.shape[0], -1) / 255
+
+
+def adjust_amount(amount, max_size):
+    if amount and amount > max_size:
+        print(
+            f"Insufficient data to fulfil request! {amount} > {max_size}"
+        )
+        amount = max_size
+    return amount
+
+
 class MNISTHandler:
     def __init__(self):
         mnist = tf.keras.datasets.mnist
@@ -13,36 +26,25 @@ class MNISTHandler:
             (self.test_input, self.test_output),
         ) = mnist.load_data()
 
-    def get_adjusted_input(self, array):
-        return array.reshape(array.shape[0], -1) / 255
-
-    def adjust_amount(self, amount, max_size):
-        if amount and amount > max_size:
-            print(
-                f"Insufficient data to fulfil request! {amount} > {max_size}"
-            )
-            amount = max_size
-        return amount
-
     def get_train_input(self, amount=None):
-        amount = self.adjust_amount(amount, MAX_TRAIN_SIZE)
-        adjusted_input = self.get_adjusted_input(self.train_input)
+        amount = adjust_amount(amount, MAX_TRAIN_SIZE)
+        adjusted_input = get_adjusted_input(self.train_input)
         return adjusted_input[:amount] if amount else adjusted_input
 
     def get_train_output(self, amount=None):
-        amount = self.adjust_amount(amount, MAX_TRAIN_SIZE)
+        amount = adjust_amount(amount, MAX_TRAIN_SIZE)
         output = np.zeros((len(self.train_output), 1, 10))
         for i, label in enumerate(self.train_output):
             output[i][0][label] = 1
         return output[:amount] if amount else output
 
     def get_test_input(self, amount=None):
-        amount = self.adjust_amount(amount, MAX_TEST_SIZE)
-        adjusted_input = self.get_adjusted_input(self.test_input)
+        amount = adjust_amount(amount, MAX_TEST_SIZE)
+        adjusted_input = get_adjusted_input(self.test_input)
         return adjusted_input[:amount] if amount else adjusted_input
 
     def get_test_output(self, amount=None):
-        amount = self.adjust_amount(amount, MAX_TEST_SIZE)
+        amount = adjust_amount(amount, MAX_TEST_SIZE)
         output = np.zeros((len(self.test_output), 1, 10))
         for i, label in enumerate(self.test_output):
             output[i][0][label] = 1
