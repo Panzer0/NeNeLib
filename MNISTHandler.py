5202c66455429b0d44308c9e879a0ded93c31d5b
Daniel Wieczorek <daniel.pazur@gmail.com>
2023-04-07 00:42:32 +0200

    Removed unnecessary code repetition

--- MNISTHandler.py
+++ MNISTHandler.py
@@ -16,41 +16,33 @@ class MNISTHandler:
     def get_adjusted_input(self, array):
         return array.reshape(array.shape[0], -1) / 255
 
-    def get_train_input(self, amount=None):
-        if amount and amount > MAX_TRAIN_SIZE:
+    def adjust_amount(self, amount, max_size):
+        if amount and amount > max_size:
             print(
-                f"Insufficient data to fulfil request! {amount} > {MAX_TRAIN_SIZE}"
+                f"Insufficient data to fulfil request! {amount} > {max_size}"
             )
-            amount = MAX_TRAIN_SIZE
+            amount = max_size
+        return amount
+
+    def get_train_input(self, amount=None):
+        amount = self.adjust_amount(amount, MAX_TRAIN_SIZE)
         adjusted_input = self.get_adjusted_input(self.train_input)
         return adjusted_input[:amount] if amount else adjusted_input
 
     def get_train_output(self, amount=None):
-        if amount and amount > MAX_TRAIN_SIZE:
-            print(
-                f"Insufficient data to fulfil request! {amount} > {MAX_TRAIN_SIZE}"
-            )
-            amount = MAX_TRAIN_SIZE
+        amount = self.adjust_amount(amount, MAX_TRAIN_SIZE)
         output = np.zeros((len(self.train_output), 1, 10))
         for i, label in enumerate(self.train_output):
             output[i][0][label] = 1
         return output[:amount] if amount else output
 
     def get_test_input(self, amount=None):
-        if amount and amount > MAX_TEST_SIZE:
-            print(
-                f"Insufficient data to fulfil request! {amount} > {MAX_TEST_SIZE}"
-            )
-            amount = MAX_TEST_SIZE
+        amount = self.adjust_amount(amount, MAX_TEST_SIZE)
         adjusted_input = self.get_adjusted_input(self.test_input)
         return adjusted_input[:amount] if amount else adjusted_input
 
     def get_test_output(self, amount=None):
-        if amount and amount > MAX_TEST_SIZE:
-            print(
-                f"Insufficient data to fulfil request! {amount} > {MAX_TEST_SIZE}"
-            )
-            amount = MAX_TEST_SIZE
+        amount = self.adjust_amount(amount, MAX_TEST_SIZE)
         output = np.zeros((len(self.test_output), 1, 10))
         for i, label in enumerate(self.test_output):
             output[i][0][label] = 1
