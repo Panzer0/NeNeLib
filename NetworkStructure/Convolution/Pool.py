3ddfb4c00afde1225e398b3bff617d58fab96dbb
Daniel Wieczorek <daniel.pazur@gmail.com>
2023-04-23 00:07:38 +0200

    Implemented forward-propagation pooling

--- NetworkStructure/Convolution/Pool.py
+++ NetworkStructure/Convolution/Pool.py
@@ -1,57 +1,68 @@
-from math import sqrt
-
 import numpy as np
 
-### PROCEDURE
-#   Split input
-#   Multiply by filters
-from ActivationFunctions.ReLU import ReLU
 
-dim = 3
+def sum_tuples(first, second):
+    return tuple(a + b for a, b in zip(first, second))
 
 
-####    Below is a skeleton of the entire CNN rather than just the data layer
-
-
-def calc_kernel_layer(sections, kernels):
-    return np.dot(sections, kernels.T)
-
-
-class Conv:
-    def __init__(self, filters=None, stride=1):
-        if filters is None:
-            self.filters = np.random.uniform(low=-0.5, high=0.5, size=(1, dim*dim))
-        else:
-            self.filters = filters
-        # todo: Should the two be merged? If I filters are just glorified
-        #       weights, that'd make things much easier
-        # self.weights = list()
-        self.weights = list()
-        self.filter_dim = int(sqrt(self.filters.shape[1]))
+class Pool:
+    def __init__(self, size=2, stride=2):
+        self.size = size
         self.stride = stride
-        print(self.filter_dim)
 
-    def split(self, image, size, stride=1):
+
+    def calc_h(self, image) -> int:
+        return int((image.shape[0] - self.size) / self.stride + 1)
+
+    def calc_w(self, image) -> int:
+        return int((image.shape[1] - self.size) / self.stride + 1)
+
+    def pool(self, image):
         subarrays = [
-            image[y: y + size, x: x + size].T.flatten()
-            for x in range(image.shape[1] - size + 1)[::stride]
-            for y in range(image.shape[0] - size + 1)[::stride]
+            max(max(row) for row in image[y: y + self.size, x: x + self.size])
+            for y in range(image.shape[0] - self.size + 1)[:: self.stride]
+            for x in range(image.shape[1] - self.size + 1)[:: self.stride]
         ]
-        return np.array(subarrays)
+        return np.array(subarrays).reshape(
+            self.calc_h(image), self.calc_w(image)
+        )
+
+    def get_max_indices(self, image):
+        indices = [
+            sum_tuples(
+                np.unravel_index(
+                    (np.argmax(image[y: y + self.size, x: x + self.size])),
+                    (self.size, self.size),
+                ),
+                (y, x),
+            )
+            for y in range(image.shape[0] - self.size + 1)[:: self.stride]
+            for x in range(image.shape[1] - self.size + 1)[:: self.stride]
+        ]
+        return np.array(indices).reshape(
+            self.calc_h(image), self.calc_w(image), 2
+        )
 
     def apply(self, image):
-        split_image = self.split(image, self.filter_dim, self.stride)
-        return np.dot(split_image, self.filters)
+        self.max_indices = self.get_max_indices(image)
+        return self.pool(image)
 
     # todo
     def back_propagate(self, data):
         pass
 
 
-    def __str__(self):
-        return str(self.filters)
-
-
-if __name__ == '__main__':
-    conv = Conv()
-    print(conv)
+if __name__ == "__main__":
+    image = np.array(
+        [
+            [3, 6, 7, 5, 3, 5],
+            [6, 2, 9, 1, 2, 7],
+            [0, 9, 3, 6, 0, 6],
+            [2, 6, 1, 8, 7, 9],
+            [2, 0, 2, 3, 7, 5],
+            [9, 2, 2, 8, 9, 7],
+        ]
+    )
+    pll = Pool()
+    print(pll.apply(image))
+    print(pll.max_indices)
